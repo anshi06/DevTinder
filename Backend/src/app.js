36e73df04df5084eea4560bf625ede21a2fcb728
anshi06 <aradhya6768@gmail.com>
2025-01-06 11:36:36 +0530

    API calls and http methods

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,28 +2,31 @@ const express = require("express");
 
 const app = express(); //Instance of express application, server
 
-//Handle all the any incoming request
-// app.use((req, res) => {
-//     //Request handler function
-//     res.send("Hello from the Express server!")
-// })
+app.use("/user", (req, res) => {
+    res.send("Now I will run and won't give chance to any /user http method.");
+});
 
-//Handle different request according to route
-app.use("/", (req, res) => {
-    //Request handler function
-    res.send("Hello from Dashboard!")
-})
+//This will only handle GET call to "/user"
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Anshika", lastName: "Upadhyay" });
+});
 
+//This will only handle POST call to "/user"
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully!");
+});
+
+//This will only handle DELETE call to "/user"
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully!");
+});
+
+//This will match all the http methods GET, POST, PATCH, PUT, DELETE API calls to '/test'
 app.use("/test", (req, res) => {
-    //Request handler function
-    res.send("Hello from test!")
-})
+  //Request handler function
+  res.send("Hello from test!");
+});
 
-app.use("/hello", (req, res) => {
-    //Request handler function
-    res.send("Hello!")
-})
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000", "http://localhost:3000")
-})
+app.listen(3001, () => {
+  console.log("Server is running on port 3001", "http://localhost:3001");
+});
