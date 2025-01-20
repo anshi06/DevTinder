84cabf066df49ef9c0a8fab7c9de893f2e229f8d
anshi06 <aradhya6768@gmail.com>
2025-01-20 11:09:00 +0530

    Connected to the database

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,36 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/database");
 
 const app = express(); //Instance of express application, server
 
-//We can not leave all these admin unauthenticated, we need to check if request is authrorized.
-//Use middleware logic to authorization
-app.use("/admin", (req, res, next) => {
-  //Every request route /admin will go through this middleware
-  console.log('auth middleware!')
-  const token = "abc";
-  const isAdminAuthorized = token === "abc";
-  if (!isAdminAuthorized) {
-    res.status(405).send("Not Authorized");
-  } 
-  else{
-    next();
-  }
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  //Route handler
-  throw new Error("dsfds");
-  res.send("All data sent!");
-});
-
-
-app.use("/", (err, req, res, next) => {
-  if(err){
-    console.log(err)
-    res.status(500).send("Something went wrong!")
-  }
-})
-
-app.listen(3001, () => {
-  console.log("Server is running on port 3001", "http://localhost:3001");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established!");
+    app.listen(3001, () => {
+      console.log("Server is running on port 3001", "http://localhost:3001");
+    });
+  })
+  .catch((err) => {
+    console.err("Database can not be connected!", err);
+  });
