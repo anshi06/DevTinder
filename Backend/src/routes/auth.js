3fd2787f2bf8cf7e860a519ac7e272550e37db64
anshi06 <aradhya6768@gmail.com>
2025-02-05 19:20:04 +0530

    express router

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -30,30 +30,38 @@ router.post("/signup", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-    try {
-      const { emailId, password } = req.body;
-  
-      const user = await User.findOne({ emailId: emailId });
-  
-      if (!user) {
-        throw new Error("Invalid credentials!");
-      }
-      const isPasswordValid = await user.validatePassword(password);
-  
-      if (isPasswordValid) {
-        //Get a JWT Token
-        const token = await user.getJwt();
-  
-        //Add the token to cookie and send the response back to the User
-        res.cookie("token", token);
-  
-        res.send("User logged in successfully!");
-      } else {
-        throw new Error("Invalid credentials!");
-      }
-    } catch (err) {
-      res.status(400).send("ERROR:" + err);
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      throw new Error("Invalid credentials!");
     }
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      //Get a JWT Token
+      const token = await user.getJwt();
+
+      //Add the token to cookie and send the response back to the User
+      res.cookie("token", token);
+
+      res.send("User logged in successfully!");
+    } else {
+      throw new Error("Invalid credentials!");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR:" + err);
+  }
+});
+
+router.post("/logout", async (req, res) => {
+  res
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
+    })
+    .send("User Logout successfully!");
 });
 
 module.exports = router;
--- Backend/src/routes/profile.js
+++ Backend/src/routes/profile.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
 const router = express.Router();
 
-router.get("/profile", userAuth, async (req, res) => {
+router.get("/profile/view", userAuth, async (req, res) => {
   try {
     res.send(req.user);
+  } catch (err) {
+    res.send(400).send("Something went wrong");
+  }
+});
+
+router.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    Object.keys(req.body).forEach((key) => (user[key] = req.body[key]));
+
+    await user.save();
+
+    res.json({ message: "Profile updated successfully!", data: user });
   } catch (err) {
     res.send(400).send("Something went wrong");
   }
