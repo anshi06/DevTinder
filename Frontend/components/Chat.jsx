c813cf800b1be8ed7ce089e7be27b3db303d8b62
anshi06 <aradhya6768@gmail.com>
2025-03-12 13:50:20 +0530

    Chat feature completed

--- Frontend/components/Chat.jsx
+++ Frontend/components/Chat.jsx
@@ -2,18 +2,36 @@ import { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+
 const Chat = () => {
   const { targetUserId } = useParams();
-  const { targetFirstName, targetPhotoUrl } = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const user = useSelector((store) => store.user);
-  const targetUser = {
-    _id: targetUserId,
-    firstName: targetFirstName,
-    photoUrl: targetPhotoUrl,
-  };
+  const targetFirstName = searchParams.get("targetFirstName");
+  const targetPhotoUrl = searchParams.get("targetPhotoUrl");
   const userId = user?._id;
+
+  const fetchChatMessages = async () => {
+    const chatMessages = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+    const data = chatMessages.data.messages.map((message) => {
+      return {
+        firstName: message.senderId.firstName,
+        photoUrl: message.senderId.photoUrl,
+        text: message.text,
+      };
+    });
+    setMessages((messages) => [...messages, ...data]);
+  };
+
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
 
   useEffect(() => {
     if (!userId) return;
@@ -29,9 +47,7 @@ const Chat = () => {
           firstName,
           text,
           photoUrl:
-            targetUser.firstName === firstName
-              ? targetUser.photoUrl
-              : user.photoUrl,
+            targetFirstName === firstName ? targetPhotoUrl : user.photoUrl,
         },
       ]);
     });
@@ -54,41 +70,43 @@ const Chat = () => {
 
   return (
     <>
-      <div className="lg:w-1/2 mx-auto my-5 bg-gray-100 rounded-lg p-2">
-        {messages.length > 0 &&
-          messages.map((message, index) => (
-            <div
-              className={`chat ${
-                targetUser?.firstName === message.firstName
-                  ? "chat-start"
-                  : "chat-end"
-              }`}
-              key={index}
-            >
-              <div className="chat-image avatar">
-                <div className="w-10 rounded-full">
-                  <img
-                    alt="Tailwind CSS chat bubble component"
-                    src={message?.photoUrl}
-                  />
+      <div className="h-screen flex flex-col gap-3 justify-around items-center mx-6">
+        <div className="w-full mx-10 lg:w-1/2 lg:mx-auto bg-gray-100 rounded-lg lg:p-2 h-2/3 overflow-y-scroll my-3">
+          {messages.length > 0 &&
+            messages.map((message, index) => (
+              <div
+                className={`chat ${
+                  targetFirstName === message.firstName
+                    ? "chat-start"
+                    : "chat-end"
+                }`}
+                key={index}
+              >
+                <div className="chat-image avatar">
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Tailwind CSS chat bubble component"
+                      src={message?.photoUrl}
+                    />
+                  </div>
                 </div>
+                <div className="chat-header">{message?.firstName}</div>
+                <div className="chat-bubble">{message?.text}</div>
               </div>
-              <div className="chat-header">{message?.firstName}</div>
-              <div className="chat-bubble">{message?.text}</div>
-            </div>
-          ))}
-      </div>
-      <div className=" join lg:w-1/2 mx-auto rounded-lg flex justify-between">
-        <input
-          type="text"
-          placeholder="Message"
-          className="input input-md w-full bg-gray-100"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-        />
-        <button className="btn btn-neutral join-item" onClick={sendMessage}>
-          Send
-        </button>
+            ))}
+        </div>
+        <div className=" join w-full mx-10 lg:w-1/2 lg:mx-auto rounded-lg flex justify-between h-1/3">
+          <input
+            type="text"
+            placeholder="Message"
+            className="input input-md w-full bg-gray-100"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button className="btn btn-neutral join-item" onClick={sendMessage}>
+            Send
+          </button>
+        </div>
       </div>
     </>
   );
